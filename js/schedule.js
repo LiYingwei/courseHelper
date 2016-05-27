61e5d67bee72c3d28f92a510179e5a47a0e5400e
LiYingwei <liyw14@163.com>
2016-05-27 14:25:07 +0800

    courseInfo complete

--- js/schedule.js
+++ js/schedule.js
@@ -6,7 +6,7 @@ var data = {
             strokeColor : "rgba(220,220,220,1)",
             pointColor : "rgba(220,220,220,1)",
             pointStrokeColor : "#fff",
-            data : [60,30,20,50,80,66]
+            data : [30,20,50,80,66,60]  //顺序是 通识-》专业-》政治-》专选-》六模-》基础
         },
         {
             fillColor : "rgba(151,187,205,0)",
@@ -14,9 +14,41 @@ var data = {
             pointColor : "rgba(151,187,205,0)",
             pointStrokeColor : "#fff",
             data : [100,100,100,100,100,100]
-        }
+        },
+        {
+            fillColor : "rgba(151,187,205,0)",
+            strokeColor : "rgba(151,187,205,0)",
+            pointColor : "rgba(151,187,205,0)",
+            pointStrokeColor : "#fff",
+            data : [0,0,0,0,0,0]
+        },
+
     ]
 }
+
+
+
+/*var pieData = {
+    labels: [
+        "Red",
+        "Green",
+        "Yellow"
+    ],
+    datasets: [
+        {
+            data: [300, 50, 100],
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ]
+        }]
+    };*/
 
 showschedule();
 drawraderchart();
@@ -99,16 +131,33 @@ function showChartForm(No) {
         html += '<td>' + classReference.reference[No-1][i].type + '</td>';
         html += '<td>' + classReference.reference[No-1][i].need + '</td>';
         html += '<td>' + classReference.reference[No-1][i].total + '</td>';
-        html += '<td class="className" data-toggle="modal" data-target="#courseInfo'+ (No-1) + '_' + i+'">' + classReference.reference[No-1][i].class + '</td>';
-        html += '<div class="modal fade" id="courseInfo'+ (No-1) + '_' + i+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
-                    <div class="modal-dialog" role="document">\
+        html += '<td class="className" data-toggle="modal" onclick="setTimeout(\'drawPieChart()\',50)" data-target="#courseInfo'+ (No-1) + '_' + i+'">' + classReference.reference[No-1][i].class + '</td>';
+        html += '<div class="courseInfo modal fade bs-example-modal-lg" id="courseInfo'+ (No-1) + '_' + i+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
+                    <div class="modal-dialog modal-lg" role="document">\
                     <div class="modal-content">\
                       <div class="modal-header">\
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
                         <h4 class="modal-title" id="myModalLabel">'+ classReference.reference[No-1][i].class +'</h4>\
                       </div>\
                       <div class="modal-body">\
-                        此处显示各种相关信息\
+                        <div id="basicInfo" class="row">\
+                        </div>\
+                        <div id="bbsInfo" class="row">\
+                        </div>\
+                        <div class="row">\
+                            <div class="col-xs-6">\
+                                <div id="scoreInfo" class="row">\
+                                    <h6>评分要求</h6>\
+                                </div>\
+                                <div class="row">\
+                                    <p align="center"><a href="http://xk.fudan.edu.cn/fdeams/syllabusUpload!download.action?kcdm=911.020.1&jsgh=20073"> 课程大纲下载 </a></p>\
+                                </div>\
+                            </div>\
+                            <div class="col-xs-6">\
+                                <canvas id="pieChart" class="raw" width="300px" height="300px"></canvas>\
+                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GPA分布情况</p>\
+                            </div>\
+                        </div>\
                       </div>\
                       <div class="modal-footer">\
                         <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>\
@@ -146,3 +195,107 @@ function drawraderchart() {
     //This will get the first returned node in the jQuery collection.
     var myNewChart = new Chart(ctx).Radar(data);
 }
+
+function drawPieChart() {
+    //A A- B+ B B- C+ C C- D D- F
+    var grade = [0.8,0.8,0.8,0.8];
+    var sum = 0;
+    for(var i = 0;i < grade.length; i++)
+        grade[i] += Math.random(), sum += grade[i];
+    for(var i = 0;i < grade.length; i++)
+        grade[i] = Math.floor(grade[i] * 100 / sum);
+    var pieData = [
+    {
+        value: grade[0],
+        label: 'A',
+        color: "#F38630",
+        labelColor : 'white',
+        labelFontSize : '16'
+    },
+    {
+        value : grade[1],
+        label: 'B',
+        color: "#F34353",
+        labelColor : 'white',
+        labelFontSize : '16'
+    },
+    {
+        value : grade[2],
+        label: 'C',
+        color: 'blue',
+        labelColor : 'white',
+        labelFontSize : '16'
+    },
+    {
+        value : grade[3],
+        label: 'D',
+        color: "green",
+        labelColor : 'white',
+        labelFontSize : '16'
+    }
+    ];
+    /*var pieData = [{
+                value : 30,
+                color : "#F38630",
+                label : 'Sleep',
+                labelColor : 'white',
+                labelFontSize : '16'
+            },
+                  {
+                value : 30,
+                color : "#F34353",
+                label : 'Sleep',
+                labelColor : 'white',
+                labelFontSize : '16'
+            }];*/
+    var pieOptions = {
+        segmentShowStroke : true,
+        animateScale : true,
+        inGraphDataShow: true,
+        animationSteps: 100,
+        animationEasing: 'easeInOutQuart'
+    };
+    var ctx = $("#pieChart").get(0).getContext("2d");
+    var pieChart = new Chart(ctx).Pie(pieData, pieOptions);
+
+    $('#basicInfo').html('<h6>基本信息</h6>\
+        <table class="table">\
+                                <tr>\
+                                    <th>课程序号</th>\
+                                    <td>COMP130001.01</td>\
+                                    <th>联系方式</th>\
+                                    <td>couresHelper@fudan.edu.cn</td>\
+                                </tr>\
+                                <tr>\
+                                    <th>课程名称</th>\
+                                    <td>人机交互</td>\
+                                    <th>作业量</td>\
+                                    <td>每周额外2小时</td>\
+                                </tr>\
+                                <tr>\
+                                    <th>教师</th>\
+                                    <td>丁向华</td>\
+                                    <th>考试时间</th>\
+                                    <td>2016.6.22</th>\
+                                </tr>\
+                                <tr>\
+                                    <th>时间</th>\
+                                    <td>周二8-9</td>\
+                                    <th>地点</th>\
+                                    <td>Z2333</td>\
+                                </tr> \
+                            </table>');
+    $('#bbsInfo').html('<h6>BBS评价</h6>\
+        <ul>\
+            <li>这门课虽然是计算机系专业课但是涉及心理学，社会学等多学科的交叉，一学期上下来收获很大！</li>\
+            <li>美女老师人很好！和颜悦色通情达理而且还找来新加坡的教授来给我们讲座！</li>\
+        </ul>');
+    $('#scoreInfo').html('<h6>评分要求</h6>\
+        <ul>\
+            <li>虽然实现项目码代码花了很多时间，但是报告真的很重要！！因为报告中包括了做项目的全部过程，比如纸模型，调研之类的。老师更关注这些。</li>\
+            <li>pre占25%，期中20%，期末60%，平时15%</li>\
+        </ul>');
+}
+
+
+
