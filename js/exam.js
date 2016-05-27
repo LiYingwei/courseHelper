18b9dbf3a225cee5dd7a2dbd5839c68241ef433e
instr3 <instr3@163.com>
2016-05-27 10:54:49 +0800

    Finish examcalendar.html mechanism.

--- js/exam.js
+++ js/exam.js
@@ -1,28 +1,44 @@
-var event = [
+$(".form_datetime").datetimepicker({
+    format: "yyyy-mm-dd hh:ii",
+    autoclose: true,
+    todayBtn: true,
+    pickerPosition: "bottom-left"
+});
+var examInfo = [];
+var testExamInfo = [
         {
             title  : '人机交互',
             start  : '2016-05-10',
-            time   : '8:00',
+            time   : '08:00',
             position:'Z2333',
             method : '论文'
         },
         {
             title  : '十遍含数',
             start  : '2016-05-23',
-            time   : '2:22',
+            time   : '02:20',
             position:'H2222',
             method : '闭卷'
         },
         {
             title  : '局部解剖学',
             start  : '2016-05-31',
-            time   : '12:07',
+            time   : '12:00',
             position:'F5201',
+            method : '开卷'
+        },
+        {
+            title  : '鬼畜心理学',
+            start  : '2016-05-31',
+            time   : '17:00',
+            position:'Z计算机机房1',
             method : '开卷'
         }
     ];
 
-loadCountDown(event);
+
+loadExamInfo();
+loadCountDown(examInfo);
 $('#calendar').fullCalendar({
     theme: false,
     buttonText: {
@@ -40,31 +56,11 @@ $('#calendar').fullCalendar({
         //right: 'title',
         //right: 'month'
     },
-    events: event,
+    events: examInfo,
 });
 
-function loadCountDown(event) {
-    var html='<h1>倒计时&nbsp;&nbsp;&nbsp;' + '<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModalNew">\
-  新增\
-</button></h1>';
-
-    html+='<div class="modal fade" id="myModalNew" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
-  <div class="modal-dialog" role="document">\
-    <div class="modal-content">\
-      <div class="modal-header">\
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
-        <h4 class="modal-title" id="myModalLabel">新增</h4>\
-      </div>\
-      <div class="modal-body">\
-        此处新增的表\
-      </div>\
-      <div class="modal-footer">\
-        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>\
-        <button type="button" class="btn btn-primary">保存</button>\
-      </div>\
-    </div>\
-  </div>\
-</div>';
+function loadCountDown(examInfo) {
+    var html='',htmlpast='';
     html+='<table class="table">';
     html+='<thead>\
         <tr>\
@@ -78,42 +74,104 @@ function loadCountDown(event) {
         </tr>\
         </thead>';
     html+='<tbody>';
-    for(var i=0;i<event.length;i++)
+    htmlpast=html.replace("<th>倒计时</th>","");
+    for(var i=0;i<examInfo.length;i++)
     {
         var today = new Date();
-        var time = new Date(event[i].start);
-        html += '<tr>';
-        html += '<td>' + Math.floor((time.getTime() - today.getTime())/ 86400000 + 1)  + '</td>';
-        html += '<td>' + event[i].start + '</td>';
-        html += '<td>' + event[i].time + '</td>';
-        html += '<td>' + event[i].title + '</td>';
-        html += '<td>' + event[i].position + '</td>';
-        html += '<td>' + event[i].method + '</td>';
-        html += '<td>' + '<button type="button" class="btn btn-warning btn-xs" data-toggle="modal" data-target="#myModalChange'+i+'">\
+        var time = new Date(examInfo[i].start);
+        var str="";
+        var count=Math.floor((time.getTime() - today.getTime())/ 86400000 + 1);
+        str += '<tr>';
+        if(count>=0)str += '<td>' + count;
+        str += '<td>' + examInfo[i].start + '</td>';
+        str += '<td>' + examInfo[i].time + '</td>';
+        str += '<td>' + examInfo[i].title + '</td>';
+        str += '<td>' + examInfo[i].position + '</td>';
+        str += '<td>' + examInfo[i].method + '</td>';
+        str += '<td>' + '<button type="button" class="btn btn-warning btn-xs" data-toggle="modal" data-target="#myModal" onclick="modifyExam(' + i + ')">\
   修改\
-</button>' + '&nbsp;<button type="button" class="btn btn-danger btn-xs">删除</button>'
-    +
-     '<div class="modal fade" id="myModalChange'+i+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
-    <div class="modal-dialog" role="document">\
-    <div class="modal-content">\
-      <div class="modal-header">\
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
-        <h4 class="modal-title" id="myModalLabel">修改</h4>\
-      </div>\
-      <div class="modal-body">\
-        此处一堆东西\
-      </div>\
-      <div class="modal-footer">\
-        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>\
-        <button type="button" class="btn btn-primary">保存</button>\
-      </div>\
-    </div>\
-  </div>\
-</div>'+
-'</td>';
-        html += '</tr>';
+</button>' + '&nbsp;<button type="button" class="btn btn-danger btn-xs" onclick="deleteExam(' + i + ')">删除</button>'
+    + '</td>';
+        str += '</tr>';
+        if(count>=0)html+=str;else htmlpast+=str;
     }
     html += '</tbody>';
     html += '</table>';
+    htmlpast += '</tbody>';
+    htmlpast += '</table>';
     $('#countDown').html(html);
+    $('#pastExam').html(htmlpast);
+}
+function sortExamInfo()
+{
+    examInfo.sort(function(a,b){return a.start==b.start?a.time>b.time:a.start>b.start;});
+}
+function saveExamInfo()
+{
+    localStorage["exams"]=JSON.stringify(examInfo);
+}
+function refreshPage()
+{
+    location.reload();
+}
+function loadExamInfo()
+{
+    if(localStorage["exams"]==undefined)
+    {
+        examInfo=testExamInfo;
+    }
+    else
+    {
+        examInfo=eval("["+localStorage["exams"]+"]")[0];
+    }
+}
+var modifiedItem;
+function deleteExam(examInfoIndex)
+{
+    examInfo.splice(examInfoIndex,1);
+    sortExamInfo();
+    saveExamInfo();
+    refreshPage();
+}
+function modifyExam(examInfoIndex)
+{
+    modifiedItem=examInfoIndex;
+    if(examInfoIndex>=0)
+    {
+        $("#modifyExamInfo #title").val(examInfo[examInfoIndex].title);
+        $("#modifyExamInfo #date_time").val(examInfo[examInfoIndex].start + ' ' + examInfo[examInfoIndex].time);
+        $("#modifyExamInfo #position").val(examInfo[examInfoIndex].position);
+        $("#modifyExamInfo #method").val(examInfo[examInfoIndex].method);
+    }
+}
+function commitModify()
+{
+    if(modifiedItem==-1)
+    {
+        examInfo.push({});
+        modifiedItem=examInfo.length-1;
+    }
+    examInfo[modifiedItem].title=$("#modifyExamInfo #title").val();
+    examInfo[modifiedItem].start=$("#modifyExamInfo #date_time").val().split(' ')[0];
+    examInfo[modifiedItem].time=$("#modifyExamInfo #date_time").val().split(' ')[1];
+    examInfo[modifiedItem].position=$("#modifyExamInfo #position").val();
+    examInfo[modifiedItem].method=$("#modifyExamInfo #method").val();
+
+    sortExamInfo();
+    saveExamInfo();
+    refreshPage();
+}
+function removeAllPast()
+{
+
+    for(var i=examInfo.length-1;i>=0;i--)
+    {
+        var today = new Date();
+        var time = new Date(examInfo[i].start);
+        var count=Math.floor((time.getTime() - today.getTime())/ 86400000 + 1);
+        if(count<=0)examInfo.splice(i,1);
+    }
+    sortExamInfo();
+    saveExamInfo();
+    refreshPage();
 }
