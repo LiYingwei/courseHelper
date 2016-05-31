dcda9cffe17c7f2d91d4185efd91b8518d25188c
KiraMelody <jessie_tjq@163.com>
2016-05-31 15:52:44 +0800

    hahaha

--- header/header.js
+++ header/header.js
@@ -1,6 +1,26 @@
 htmlobj=$.ajax({url:"/courseHelper/header/header.html",async:false});
 $("#common-header").html(htmlobj.responseText);
-
+$('navbarInput-01.typeahead').typeahead
+({
+	/*source: function (query, process) 
+	{
+    	$.ajax({
+      	url: '/js/courseinfo.js',
+       	type: 'POST',
+       	dataType: 'JSON',
+       	data: query,
+       	success: function(data) 
+       	{
+      		console.log(data);
+      		process(data);
+        }
+      	});
+   	}*/
+   	source: function(query, process) 
+   	{
+         return ["Deluxe Bicycle", "Super Deluxe Trampoline", "Super Duper Scooter"];
+    }
+});
 if($("#maintab1").length>0)
 {
     $('#tab1').addClass("active");
--- js/coursetable.js
+++ js/coursetable.js
@@ -9,14 +9,14 @@ function drawcoursetable() {
 	tablehtml = '<table class="table table-bordered">';
 	tablehtml += '<thead id=\"coursehead">\
               <tr>\
-              	<th> 时间 </th>\
+              	<th style = "border-top-left-radius: 20px"> 时间 </th>\
                 <th>星期日</th>\
                 <th>星期一</th>\
                 <th>星期二</th>\
                 <th>星期三</th>\
                 <th>星期四</th>\
                 <th>星期五</th>\
-                <th>星期六</th>\
+                <th style = "border-top-right-radius: 20px">星期六</th>\
               </tr>\
             </thead>';
     var selectedAtTime=[];
@@ -50,11 +50,13 @@ function drawcoursetable() {
     for(var i = 1; i <= 14; ++i)
     {
     	tablehtml+='<tr>';
-    	tablehtml+='<td>第' + i + '节</td>';
+        if(i == 14) tablehtml+='<td style = "border-bottom-left-radius: 20px">第' + i + '节</td>';
+        else tablehtml+='<td>第' + i + '节</td>';
     	for(var j = 0; j < 7; ++j)
         {
             if(selectedAtTime[i][j]<0)continue;
             tablehtml+='<td class="xkCell" unit=' + i + ' weekday=' + j;
+            if(i == 14 && j == 6) tablehtml+='<td style = "border-bottom-right-radius: 20px">';
             var arrange=selectedArrangeAtTime[i][j];
             if(selectedAtTime[i][j]>0)
             {
